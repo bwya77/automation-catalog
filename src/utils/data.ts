2b48efeaa24b8cf255dafbab0b30c247e6cf1c85
Bradley Wyatt <brad@thelazyadministrator.com>
2025-10-30 17:39:47 -0500

    adding company drop down

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -19,6 +19,7 @@ export interface Automation {
   name: string;
   author: string;
   department: string;
+  customer?: string;
   status: 'live' | 'development' | 'backlog';
   description: string;
   tags: string[];
